077374d30fe13286d0997cd80dc99998f209a50e
lbenvenutoc <lbenvenutoc@gmail.com>
2016-11-26 20:34:55 -0500

    Prueba 11

--- app/src/main/java/com/happybox/applic/actividad/BienvenidaActivity.java
+++ app/src/main/java/com/happybox/applic/actividad/BienvenidaActivity.java
@@ -9,7 +9,7 @@ import com.happybox.applic.R;
 
 public class BienvenidaActivity extends AppCompatActivity {
 
-    //BIENVENIDO 5
+    //BIENVENIDO 8
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
